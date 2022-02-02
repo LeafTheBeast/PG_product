e25414155913e595a4eae1ea2c553f501dee2e7c
Pierre <groenielp@gmail.com>
2022-02-02 20:10:06 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include_once("style.php")
+
+?>
+
+</style>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Pierre Grönich</h1>
+  <div class="container-1">
+    <div class="box-1">
+      <h3>Box One</h3>
+      <p>Das ist eine Box.</p>
+    </div>
+    <div class="box-2">
+      <h3>Box Two</h3>
+      <p>Das ist eine Box.</p>
+    </div>
+    <div class="box-3">
+      <h3>Box Three</h3>
+      <p>Das ist eine Box.</p>
+    </div>
+  </div>
+</body>
+
+</html>
+<?php
+include_once("code.php")
+?>
---
+++ style.php
@@ -0,0 +1,34 @@
+<style>
+    @media(min-width:768px) {
+        .container-1 {
+            display: flex;
+            justify-content: space-between;
+
+        }
+    }
+
+    .container-1 div {
+        border: 1px #ccc solid;
+        padding: 10px;
+
+    }
+
+    .box-1 {
+        flex-basis: 27%;
+    }
+
+    .box-1 {
+        flex: 2;
+        order: 2;
+
+    }
+
+    .box-2 {
+        flex: 1;
+        order: 1;
+    }
+
+    .box-3 {
+        flex: 1;
+        order: 3;
+    }
